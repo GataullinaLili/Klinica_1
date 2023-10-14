b7de86516e78ca0a77a308fb7bc02bb37e23ccc3
nekrochan <rubus.saxatilis.99@gmail.com>
2023-10-14 15:31:40 +0300

    ТЕКУЩАЯ ТОЧКА: добавление компонентов в рамку

--- src/KlinicaGui.java
+++ src/KlinicaGui.java
@@ -42,6 +42,33 @@ public class KlinicaGui {
             panels.add(new JPanel());
         }
 
+        //скрытие панелей кроме первой
+        panels.get(0).setVisible(true);
+        for (int i = 1; i < 9; i++){
+            panels.get(i).setVisible(false);
+        }
+
+        // добавление компонентов
+        // в (задание 3)
+        JLabel lb1 = new JLabel("Введите месяц");
+        JTextField tf1 = new JTextField(2); // принимает до 2 символов
+        JLabel lb2 = new JLabel("год");
+        JTextField tf2 = new JTextField(4); // принимает до 4 символов
+        JLabel lb3 = new JLabel("ФИО врача");
+        JTextField tf3 = new JTextField(20); // принимает до 20 символов
+        JButton bt31 = new JButton("Поиск");
+
+        // Создание текстовых областей
+        for (int i = 0; i < 5; i++){
+            panels.get(i).add(new JTextArea());
+        }
+
+        // Добавление компонентов в рамку.
+        frame.getContentPane().add(BorderLayout.NORTH, menuItems.get(0));
+        frame.getContentPane().add(BorderLayout.SOUTH, panels.get(0));
+        frame.getContentPane().add(BorderLayout.CENTER, panels.get(0).getComponent(0));// Текстовая область по центру
+        frame.setVisible(true);
+
 
     }
 }
